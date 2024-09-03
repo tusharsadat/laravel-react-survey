215c41ef44e61a3734e446cc9f5162cb29f3b600
tushar-sadat <tusharsadat30@gmail.com>
2024-09-04 03:58:41 +0600

    install axios and create surveyView page

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -5,6 +5,7 @@ import Login from "./views/Login";
 import Signup from "./views/Signup";
 import GuestLayout from "./components/GuestLayout";
 import DefaultLayout from "./components/DefaultLayout";
+import SurveyView from "./views/SurveyView";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
       {
         path: "/survey",
         element: <Survey />,
+      },
+      {
+        path: "/surveys/create",
+        element: <SurveyView />,
       },
     ],
   },
--- react/src/views/Survey.jsx
+++ react/src/views/Survey.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import PageComponent from "../components/PageComponent";
 import { useStateContext } from "../contexts/ContextProvider.jsx";
 import SurveyListItem from "../components/SurveyListItem";
+import TButton from "../core/TButton.jsx";
 
 const Survey = () => {
   const { tmpSurveys } = useStateContext();
@@ -9,7 +10,15 @@ const Survey = () => {
   const onDeleteClick = () => {};
 
   return (
-    <PageComponent title="Survey">
+    <PageComponent
+      title="Survey"
+      buttons={
+        <TButton color="green" to="/surveys/create">
+          <PlusCircleIcon className="h-6 w-6 mr-2" />
+          Create new
+        </TButton>
+      }
+    >
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
         {tmpSurveys.map((survey) => (
           <SurveyListItem
