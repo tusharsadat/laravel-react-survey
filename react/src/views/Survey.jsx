e2a67b0464c5a61fea5f21cfcf934bce535fcef4
tushar-sadat <tusharsadat30@gmail.com>
2024-09-03 21:15:50 +0600

    survey list item show

--- react/src/views/Survey.jsx
+++ react/src/views/Survey.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import PageComponent from "../components/PageComponent";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
+import SurveyListItem from "../components/SurveyListItem";
 
 const Survey = () => {
-  return <PageComponent title="Survey"></PageComponent>;
+  const { tmpSurveys } = useStateContext();
+
+  const onDeleteClick = () => {};
+
+  return (
+    <PageComponent title="Survey">
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+        {tmpSurveys.map((survey) => (
+          <SurveyListItem
+            survey={survey}
+            key={survey.id}
+            onDeleteClick={onDeleteClick}
+          />
+        ))}
+      </div>
+    </PageComponent>
+  );
 };
 
 export default Survey;
